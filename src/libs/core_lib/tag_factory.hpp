82ecdc9f6bf6af484a02f9373d1498a4836af5a8
Joseph Metcalf <jlemetcalf@gmail.com>
2022-01-06 21:11:06 +1100

    Added in ability to get a collection of unique tags by tag name

--- src/libs/core_lib/tag_factory.hpp
+++ src/libs/core_lib/tag_factory.hpp
@@ -10,13 +10,38 @@ namespace core {
 class TagFactory
 {
 public:
+  using TagMap = std::unordered_map<std::string, std::unordered_set<Tag, TagHash>>;
   TagFactory() = default;
-  Tag CreateTag(const std::string& name, const std::string& value)
+  
+  // Adding in nodiscard to make sure tags are not accidentally created and just fill up the cache.
+  [[nodiscard]] Tag CreateTag(const std::string& name, const std::string& value)
   {
-    return Tag(mCache.GetOrCache(name)->get(), mCache.GetOrCache(value)->get());
+    auto cachedName = mCache.GetOrCache(name);
+    auto cachedValue = mCache.GetOrCache(value);
+    auto tag = Tag(cachedName->get(), cachedValue->get());
+    AddUniqueTag(name, tag);
+    return tag;
   }
+
+  const TagMap& GetUniqueTags() const
+  {
+    return mUniqueTags;
+  }
+
 protected:
+  void AddUniqueTag(const std::string& name, const Tag& tag)
+  {
+    auto findIt = mUniqueTags.find(name);
+    if (findIt == mUniqueTags.end())
+    {
+      findIt = mUniqueTags.insert(std::make_pair(name, std::unordered_set<Tag, TagHash>())).first;
+    }
+    findIt->second.insert(tag);
+  }
+
+private:
   StringCache mCache;
+  TagMap mUniqueTags;
 };
 
 }// namespace core
--- test/entity_store_tests.cpp
+++ test/entity_store_tests.cpp
@@ -54,3 +54,36 @@ TEST_CASE("Entity Store Lookup")
   REQUIRE(entities2.size() == 1);
   REQUIRE(std::find(entities2.begin(), entities2.end(), 3) != entities2.end());
 }
+
+TEST_CASE("Entity Store Get Unique Tags")
+{
+  core::TagFactory tagFactory;
+  core::EntityStore<u_int64_t> store;
+
+  [[maybe_unused]] auto tag1 = tagFactory.CreateTag("TagName1", "TagValue1-1");
+  [[maybe_unused]] auto tag2 = tagFactory.CreateTag("TagName1", "TagValue1-2");
+  [[maybe_unused]] auto tag3 = tagFactory.CreateTag("TagName2", "TagValue2-1");
+  [[maybe_unused]] auto tag4 = tagFactory.CreateTag("TagName2", "TagValue2-2");
+
+  [[maybe_unused]] auto duplicateTag1 = tagFactory.CreateTag("TagName1", "TagValue1-1");
+  [[maybe_unused]] auto duplicateTag2 = tagFactory.CreateTag("TagName1", "TagValue1-2");
+  [[maybe_unused]] auto duplicateTag3 = tagFactory.CreateTag("TagName2", "TagValue2-1");
+  [[maybe_unused]] auto duplicateTag4 = tagFactory.CreateTag("TagName2", "TagValue2-2");
+
+  const auto& uniqueTags = tagFactory.GetUniqueTags();
+
+  REQUIRE(uniqueTags.size() == 2);
+
+  auto checkTagsLambda = [&uniqueTags] (const std::string_view tagName, std::unordered_set<std::string> values) -> void {
+    const auto tagNameIt = uniqueTags.find(std::string(tagName));
+    REQUIRE(tagNameIt != uniqueTags.end());
+    REQUIRE(tagNameIt->second.size() == values.size());
+    std::for_each(tagNameIt->second.begin(), tagNameIt->second.end(), [&values] (const core::Tag& tag) {
+      values.erase(tag.Value());
+    });
+    REQUIRE(values.size() == 0);
+  };
+
+  checkTagsLambda("TagName1", { "TagValue1-1", "TagValue1-2"});
+  checkTagsLambda("TagName2", { "TagValue2-1", "TagValue2-2"});
+}
