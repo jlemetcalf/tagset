1213a3acbe3525d05566ea7afcae0571629c2af4
Joseph Metcalf <jlemetcalf@gmail.com>
2022-08-24 20:24:16 +1000

    Yet another test

--- test/tag_tests.cpp
+++ test/tag_tests.cpp
@@ -6,47 +6,47 @@
 
 #include "tag_factory.hpp"
 
-// TEST_CASE("Tag Simple Test")
-// {
-//   core::TagFactory factory;
+TEST_CASE("Tag Simple Test")
+{
+  core::TagFactory factory;
 
-//   auto one = factory.CreateTag("Name1", "Value1");
-//   auto two = factory.CreateTag("Name2", "Value2");
-//   auto three = factory.CreateTag("Name3", "Value3");
+  auto one = factory.CreateTag("Name1", "Value1");
+  auto two = factory.CreateTag("Name2", "Value2");
+  auto three = factory.CreateTag("Name3", "Value3");
 
-//   REQUIRE("Name1" == one.Name());
-//   REQUIRE("Value1" == one.Value());
-//   REQUIRE("Name2" == two.Name());
-//   REQUIRE("Value2" == two.Value());
-//   REQUIRE("Name3" == three.Name());
-//   REQUIRE("Value3" == three.Value());
-// }
+  REQUIRE("Name1" == one.Name());
+  REQUIRE("Value1" == one.Value());
+  REQUIRE("Name2" == two.Name());
+  REQUIRE("Value2" == two.Value());
+  REQUIRE("Name3" == three.Name());
+  REQUIRE("Value3" == three.Value());
+}
 
-// TEST_CASE("Tag Large Test")
-// {
-//   core::TagFactory factory;
+TEST_CASE("Tag Large Test")
+{
+  core::TagFactory factory;
 
-//   std::vector<core::Tag> tags;
-//   std::vector<std::pair<std::string, std::string>> randomTagStrings;
-//   std::srand((unsigned int) std::time(nullptr)); // use current time as seed for random generator
-//   const auto max = 1000;
-//   auto count = max;
-//   while (count > 0)
-//   {
-//     int randomValue = std::rand();
-//     auto nameStr = fmt::format("Name-{}", randomValue);
-//     auto valueStr = fmt::format("Value-{}", randomValue);
+  std::vector<core::Tag> tags;
+  std::vector<std::pair<std::string, std::string>> randomTagStrings;
+  std::srand((unsigned int)std::time(nullptr));// use current time as seed for random generator
+  const auto max = 1000;
+  auto count = max;
+  while (count > 0)
+  {
+    int randomValue = std::rand();
+    auto nameStr = fmt::format("Name-{}", randomValue);
+    auto valueStr = fmt::format("Value-{}", randomValue);
 
-//     tags.push_back(factory.CreateTag(nameStr, valueStr));
-//     randomTagStrings.push_back(std::make_pair(nameStr, valueStr));
-//     count--;
-//   }
+    tags.push_back(factory.CreateTag(nameStr, valueStr));
+    randomTagStrings.push_back(std::make_pair(nameStr, valueStr));
+    count--;
+  }
 
-//   for (std::size_t idx = 0; idx < max; idx++)
-//   {
-//     const auto& tag = tags[idx];
-//     const auto& tagStrings = randomTagStrings[idx];
-//     REQUIRE(tag.Name() == tagStrings.first);
-//     REQUIRE(tag.Value() == tagStrings.second);
-//   }
-// }
+  for (std::size_t idx = 0; idx < max; idx++)
+  {
+    const auto& tag = tags[idx];
+    const auto& tagStrings = randomTagStrings[idx];
+    REQUIRE(tag.Name() == tagStrings.first);
+    REQUIRE(tag.Value() == tagStrings.second);
+  }
+}
